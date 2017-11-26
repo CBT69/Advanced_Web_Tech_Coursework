f739fb57575723df991dc809cefbb43648b6fa19
CBT69 <31957747+CBT69@users.noreply.github.com>
2017-11-26 01:45:58 +0000

    updated pages matching the `scrit

--- flipper.php
+++ flipper.php
@@ -26,28 +26,237 @@
 			</div>
 		</div>
 	</div>
+	<div>
+		<div id="authors" class="row">
+			<div class="col">
+				<h3></h3>
+				<h4></h4>
+			</div>
+			</div>
+	</div>
+	<div>
+		<div id="authors" class="row">
+			<div class="col">
+				<h3>Edinburgh Napier University</h3>
+				<h4>27 November 2017</h4>
+			</div>
+		</div>
+	</div>
+	<div>
+		<div id="authors" class="row">
+			<div class="col">
+				<h3></h3>
+				<h4></h4>
+			</div>
+		</div>
+	</div>
+
 
 	<div>
-	 <div class="row text">
-		 <div class="row">
-			 <img src="img/cowboy4.jpg" class="img img-responsive"/>
-		 </div>
-		 <h5>The story starts with a description of the cowboy (listener) wandering
-			 through a desert with his horse and revolver as
-			 he makes his way towards the nearest town. He is stopped by an old Indian
-			 chief who asks a favour of him. The cowboy is asked to go in the mine to
-			 retrieve an old drum and may keep any of the copper, silver, and gold he finds along the way.
-			 Indian gives him his headdress to protect him from the hyenas.</h5>
-	 </div>
+		<div id="p1" class="row text">
+			<div class="col">
+					<h5>
+						A cowboy came trotting through the vast desert on as he longed to
+						reach the next town. His journey had been long and dusty but with his
+						 horse (horse noise) by his side and his revolver (pistol noise) in
+						 its holster he knew he was prepared for anything. As he travelled
+						 on he noticed a figure standing in the distance.
+					</h5>
+			</div>
+			
+			<div class="pnumberR">
+				<h6>1</h6>
+			</div>
+		</div>
+	</div>
+	<div>
+		<div id="p2" class="row text">
+			<div class="col">
+				<h5>
+					Upon closer inspection he noticed an old Indian chief standing next
+					to a mine. “Oh great cowboy, how happy I am to see you” cried the
+					Indian. “You must help me retrieve my sacred drum from this old mine
+					and in return you may keep any of the copper, silver, and gold you
+					find!” The cowboy thought about the offer before jumping off his
+					horse and walking (spurs noise) over to the Indian chief.
+					He smiled at the old Indian before exclaiming “I would be happy to
+					help!” and off he went towards the mine. “Wait!” shouted the Indian,
+					running after the cowboy. “Please take my head dress, it will protect
+					 you from the monsters that protect your reward”. The cowboy nodded
+					 politely before tucking the headdress under his arm and walking
+					 (spurs noise) into the cave.
+				</h5>
+			</div>
+
+			<div class="pnumberL">
+				<h6>1</h6>
+			</div>
+		</div>
 	</div>
 
 	<div>
-		<div class="row text">
+		<div id="p3" class="row text">
+			<div class="col">
+					<h5>
+						As the cowboy entered the mine he could hear the wind howling
+						(wind noise) behind him, pushing him deeper and deeper. Soon the
+						cowboy approached a hyena that was guarding a small chest. The hyena
+						stared and growled (hyena noise) at the cowboy, its eyes the size of
+						bullets showing no fear. The cowboy thought for a minute before
+						putting the headdress on and slowly walking towards the hyena. In an
+						instant the hyena jumped aside, inviting the cowboy to the chest.
+						Upon seeing all the copper in the chest the cowboy filled his pockets
+						before proceeding further into the mine. It was not long before the
+						cowboy came across another hyena, this one with eyes the size of
+						cart wheels. The cowboy slowly crept towards the medium sized chest
+						that was under the careful watch of the hyena before opening it to
+						reveal more silver than he had ever seen before. It didn’t take long
+						to empty all the copper out of his pockets (jingle noise) and then the
+						 cowboy shovelled as much silver as he could in his boots and pockets.
+					</h5>
+			</div>
+		</div>
+	</div>
+	<div>
+		<div id="p4" class="row text">
+			<div class="col">
+					<h5>
+						Continuing deeper into the mine the cowboy came across the largest
+						hyena he had ever seen with eyes the size of a church. The
+						cowboy was frozen with fear as he gazed at the mighty hyena but in
+						the corner of the room he could see a huge chest bursting open with
+						gold. Shaking with fear, the cowboy moved towards the chest, praying
+						the old headdress would keep the hyena away. Sure enough the hyena
+						left him as he opened the chest to reveal a mountain of gold. The
+						cowboy filled his hat, boots, and pockets with gold before noticing
+						something at the bottom of the chest. He dug deeper to reveal an old
+						and worn bongo drum. The cowboy quickly grabbed it before rushing for
+						 the exit of the mine, jingling all the way (jingle noise).
+					</h5>
+			</div>
+		</div>
+	</div>
+
+	<div>
+		<div id="p5" class="row text">
+			<div class="col">
+				<h5>
+					“I have returned old Indian” shouted the cowboy as he emerged from
+					the mine. “Before I give you this drum I must know why you needed it
+					so desperately” announced the cowboy. The old Indian looked away
+					before mumbling “I cannot tell you”. The cowboy was unhappy with
+					the answer and threatened “I shall give you once chance to tell me
+					and if you don’t then I shall have to shoot you”. The old Indian
+					refused to meet the gaze of the cowboy and shook his head while
+					holding his hand out for the drum. “Very well, you leave me no
+					choice” said the cowboy, before drawing his revolver and shooting
+					the Indian in the head (gun noise). The cowboy casually stepped over
+					the body of the Indian before strapping the drum onto his horse and
+					trotting off into the sunset (horse noise).
+				</h5>
+			</div>
+		</div>
+	</div>
+	<div>
+		<div id="p6" class="row text">
+			<div class="col">
+				<h5>
+					As the cowboy entered town he noticed the saloon bar was offering a
+					room to rent. He tied up his horse and walked towards the entrance,
+					spurs jingling as he walked (jingle noise). The cowboy approached
+					the bar (background chatter noise) and asked the barman for the
+					finest room available with the nicest meal he could prepare.
+					As the cowboy was making his way to his room he overheard some men
+					discussing the sheriff’s daughter, who was kept on a far out ranch
+					due to anyone who saw her beauty falling in love with her. The cowboy
+					thought about how beautiful she must be as he lay in bed and wishes
+					he could meet her one day. Each day the cowboy would spend his gold
+					frivolously as he gave his new found wealth to anyone and everyone
+					around him but every night his loneliness would make him think
+					of the sheriff’s daughter.
+				</h5>
+			</div>
+		</div>
+	</div>
+
+	<div>
+		<div id="p7" class="row text">
+			<div class="col">
+				<h5>
+					One evening the cowboy sat alone in his room, wishing for
+					some form of companionship, when he reached for the old drum
+					that had been gathering dust in the corner ever since he arrived.
+					He slowly started to drum (drum noise) before getting quicker and
+					quicker as he felt a strange energy flowing through him. Before
+					long the first hyena (hyena noise) he encountered appeared before
+					him and said “what is your command, master?” The cowboy could not
+					believe his luck and asked to see the most beautiful girl in the
+					world, the sheriff’s daughter. In an instant, the hyena disappeared
+					 and returned with a girl asleep on his back. The cowboy was
+					 speechless as he stared at this beautiful girl the hyena had
+					 brought. He leaned in to kiss her (kiss noise) before
+					  asking the hyena to return her to the far away ranch.
+				</h5>
+			</div>
+		</div>
+	</div>
+	<div>
+		<div id="p8" class="row text">
+			<div class="col">
+				<h5>
+					The next morning the sheriff’s daughter woke and ran downstairs
+					to tell her parents about the wonderful dream she had. While the
+					sheriff listened he pretended to be happy for his daughter but deep
+					down was worried about her. He asked one of his officers to keep a
+					watch over his daughter during the next few nights to ensure she
+					was safe. As the officer was watching the daughter sleep he could
+					not believe his eyes when a hyena appeared in front of him and put
+					the girl on his back before running off into the night. The office
+					gave chase and followed the hyena all the way back to the saloon
+					where he left a bottle of whisky outside of to allow him to show the
+					sheriff the next day. As he crept away the hyena noticed his plan
+					and proceeded to put a bottle of whiskey outside
+				</h5>
+			</div>
+		</div>
+	</div>
+
+	<div>
+	 <div id="p9" class="row text">
+		 <div class="row">
+			 <img src="img/cowboy4.jpg" class="img img-responsive"/>
+		 </div>
+		 <h5>
+			 The next morning the officer told the sheriff about what had happened
+			 and lead him into town to show him who had stolen his daughter during
+			 the night. Unfortunately every door had a bottle of whisky outside now
+			 to the officer’s surprise. He looked at the sheriff and tried to stutter
+			  an explanation but the sheriff would not listen and fired him on the
+				spot. The sheriff stormed back to the ranch to make sure his daughter
+				was still locked away from anyone who could fall in love with her and
+				as he made his way back he came up with an idea to track his daughter
+				 at night. By tying a small bag of gunpowder around her neck and
+				 cutting a small hole in the bottom, he knew he could follow the
+				 trail the next day to catch whoever was stealing her away. That
+				 night, when the hyena returned to take the girl, the little bag
+				 around her neck went unnoticed as she was carried to the saloon
+				  which left a trail all through the town.</h5>
+	 </div>
+	</div>
+	<div>
+		<div id="p10" class="row text">
 			<div>
-				<h5 class="col col-lg-6 col-md-6 col-sm-6">Cowboy goes into mine and as he goes deeper, he progresses through each of the hyenas
-				 who start aggressive but then become passive when they see the headdress.
-				 Once he finds the drum in the last room he returns to the old Indian chief and asks why the drum is needed.
-				 After the Indian won’t tell him he shoots him with his revolver.</h5>
+				<h5 class="col col-lg-6 col-md-6 col-sm-6">
+					With excitement and nervousness, the sheriff and three guards
+					followed the trail of gunpowder through town. They arrived at the
+					saloon (background chatter noise) to be greeted by the bar tender
+					who asked how he could help. The sheriff demands to search the rooms
+					for a hyena that has been stealing his daughter away each night
+					before running up the stairs, followed closely by his armed guards
+					(footsteps noise). They burst into the cowboy’s room before
+					handcuffing him and dragging him away to the local jail where he
+					 was told that when the sun rises he will be executed in the street.
+				</h5>
 			</div>
 			<div class="col col-lg-6 col-md-6 col-sm-6">
 				<img src="img/cowboy2.jpg" class="img img-responsive"/>
@@ -59,42 +268,50 @@
 	</div>
 
 	<div>
-		<div class="row text">
+		<div id="p11" class="row text">
 			<div class="col col-lg-4 col-md-4 col-sm-4">
 				<img src="img/cowboy.jpg" class="img img-responsive"/>
 			</div>
 			<div class="col col-lg-8 col-md-8 col-sm-8">
-			 <h5>Continues into town where he stops at the saloon with his newfound wealth
-			 and orders a room to stay with lots of food and drink.
-			 Overhears old men talking about the beautiful sheriff’s daughter who is locked away on a far out ranch.
-			 Unable to find a way to see her the cowboy eventually runs out of money and has to sleep behind a house
-			 in the town. As a way of trying to make money he plays the drum which summons the hyenas who offer to
-			 do what he asks. They bring him money and he goes back to living well at the saloon.</h5>
+			 <h5>As the night continued on the cowboy started to become more and
+				 more worried about how he was going to escape from his impending
+				 death. Then he realised what he needed to do and asked the guard to
+				 fetch his drum so that he may say a final prayer. The guard could not
+				 refuse such a reasonable request so sent someone to fetch the drum
+				 from the saloon but warned the cowboy that he could not play until
+				 the morning. By this point the cowboy knew all he could do was wait
+				 so he decided to rest until the morning (snoring noise).</h5>
 		 	</div>
 		 </div>
 	</div>
 
 	<div>
-		<div class="col col-lg-12 col-md-12">
+		<div id="p12" class="col col-lg-12 col-md-12">
 			<img src="img/cowboy3.jpg" class="img img-responsive image"/>
 		</div>
 		<div class="row text">
-			 <h5>Eventually asks the dogs to bring the sheriff’s daughter who appears and he kisses her.
-			 The daughter tells her father of the dream. Next night the sheriff has an officer
-			 watch over his daughter and follows the hyena as he brings the girl to the cowboy.
-			 The officer marks where the dog goes by leaving a bottle of whisky at the door but the hyena then
-			 copies this and puts whisky at every door in town.
-			 After this fails the sheriff uses a trail of gunpowder to track where the hyena takes his
-			 daughter and so he can follow it the next day.</h5>
+			 <h5>As the sun started to rise the sheriff marched into the jail and
+				 threw a bucket of water over the sleeping cowboy, shouting “your
+				 time is up, cowboy.” He dragged the cowboy out into the street in
+				 front of a crowed of on-lookers. “Please, let me say one final prayer”
+				  begged the cowboy. The sheriff looked displeased but knew he had to
+					grant a dying man’s last wish. “Very well” he mumbled, while handing
+					the drum to the cowboy. The cowboy’s eyes lit up as he started to
+					drum, faster and faster to summon forth all three hyenas (hyena noise).</h5>
 		 </div>
 	</div>
 	<div>
-		<div class="row text">
-			 <h5>Cowboy gets arrested and is thrown in jail to be executed at sun rise the next day.
-			 Asks the guard to send someone to retrieve his drum so he may say a final prayer the next day.
-			 As he is to be shot, he plays the drum to summon the hyenas who kills the sheriff
-			 and officers and free the daughter.
-			 All the townspeople are happy to let him free as he gave everyone whisky earlier. </h5>
+		<div id="p11" class="row text">
+			 <h5>
+				 “Attack!” he screeched as he pointed towards the sheriff and his
+				 guards. The hyenas leapt forward and tore the sheriff and his guards
+				  into hundreds of tiny pieces (slashing noises). The daughter looked
+					on, cowering in fear at the bloodshed, but she felt a sense of
+					freedom to finally escape her father’s rule. The cowboy slowly
+					walked over to her, went down on and knee, and proposed to her in
+					 front of the whole town who cheered for them as the hyenas danced
+					 around them.
+			 </h5>
 		</div>
 		<div class="col col-lg-12 col-md-12">
 				<img src="img/giphy.webp" class="img img-responsive image"/>
--- inc/menu.php
+++ inc/menu.php
@@ -1,12 +1,11 @@
 <div class="center">
   <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-    <!-- Previous button -->
-    <button class="button" onclick='$("#flipbook").turn("previous");'><h3 class="shade">Prevous page</h3></button>
+    <a class="button" onclick='$("#flipbook").turn("previous");'><h3 class="shade">Previous page</h3></a>
   </div>
   <div class="col col-lg-4 col-md-4 col-sm-4 col-xs-4">
     <h1 class="shade"><b>Cowboy Story</b></h1>
   </div>
   <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-    <button class="button" onclick='$("#flipbook").turn("next");'><h3 class="shade">Next page</h3></button>
+    <a class="button" onclick='$("#flipbook").turn("next");'><h3 class="shade">Next page</h3></a>
   </div>
 </div>
